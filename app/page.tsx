bf222f65e5f96e4d9ca60246c487a775cfea5909
nollae <dudghk0924@naver.com>
2025-04-06 15:00:27 +0900

    3.5 Responsive Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
 export default function Home() {
   return (
-    <main className="bg-gray-100 h-screen flex items-center justify-center p-5">
+    <main className="bg-gray-100 
+      sm:bg-red-100 
+      md:bg-green-100
+      lg:bg-cyan-100
+      xl:bg-orange-100
+      2xl:bg-purple-100
+      h-screen flex items-center justify-center p-5">
       <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm 
-        flex flex-col gap-2">
+        flex flex-col 
+        md:flex-row
+        gap-2">
         <input className='w-full rounded-full h-10 bg-gray-200 pl-5
           outline-none ring ring-orange-300 ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow' type='text' placeholder='Search here...'/>
         <button className='bg-black bg-opacity-90 text-white py-2 rounded-full active:scale-90 transition-transform placeholder:drop-shadow
-          font-medium focus:scale-90 outline-none'>
+          font-medium focus:scale-90 outline-none
+          md:px-10
+          '>
           Search
         </button>
       </div>
