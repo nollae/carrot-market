689af12b7abbce5eaf681fdd246689e778a3caa4
nollae <dudghk0924@naver.com>
2025-04-06 17:08:18 +0900

    3.6 Form Modifiers

--- app/page.tsx
+++ app/page.tsx
@@ -12,12 +12,24 @@ export default function Home() {
         md:flex-row
         gap-2">
         <input className='w-full rounded-full h-10 bg-gray-200 pl-5
-          outline-none ring ring-orange-300 ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow' type='text' placeholder='Search here...'/>
-        <button className='bg-black bg-opacity-90 text-white py-2 rounded-full active:scale-90 transition-transform placeholder:drop-shadow
+          outline-none ring ring-orange-300 ring-transparent focus:ring-green-500 focus:ring-offset-2 transition-shadow
+          invalid:focus:ring-red-500
+          peer
+          ' type='email' 
+          required
+          placeholder='Search here...'/>
+        <span className='text-red-500 font-medium
+          hidden peer-invalid:block'>
+          Email is required.
+        </span>
+        <button className='bg-black bg-opacity-90 text-white py-2 rounded-full active:scale-90 transition-transform 
+          placeholder:drop-shadow
           font-medium focus:scale-90 outline-none
           md:px-10
+          peer-invalid:bg-red-100
+          peer-required:bg-green-500
           '>
-          Search
+          Log in
         </button>
       </div>
     </main>
